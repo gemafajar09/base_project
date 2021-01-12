6ba46d057b6e33d2ae4118a95d0b86b0be0ae6ec
syahroel712 <syahroel712@gmail.com>
2021-01-12 11:13:06 +0700

    #14 REGISTER

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,34 @@ Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 // =======================LOGIN============================================
 Route::get('/admin', 'Auth\LoginController@index')->name('admin')->middleware('authcek');
 Route::get('/dashboard', 'Backend\BackendController@index')->name('dashboard')->middleware('ceklogin');
+// =======================REGISTER=========================================
+Route::get('/admin/register', 'Auth\LoginController@register')->name('admin.register');
+Route::post('/admin/aksiregister', 'Auth\LoginController@aksiregister')->name('admin.aksiregister');
+
+
 // =======================ENGINE============================================
 Route::get('/akses', 'Engine\AksesController@index')->name('akses')->middleware('ceklogin');
 
-// CRUD MENU LEVEL 1
-// Route::get('/menu-engine', 'Engine\EngineController@index')->name('menu-engine')->middleware('ceklogin');
-// Route::get('/menu-engine/add', 'Engine\EngineController@add')->name('menu-engine.add')->middleware('ceklogin');
-// Route::get('/menu-engine/edit/{id}', 'Engine\EngineController@edit')->name('menu-engine.edit')->middleware('ceklogin');
-// Route::post('/menu-engine/save', 'Engine\EngineController@save')->name('menu-engine.save')->middleware('ceklogin');
-// Route::delete('/menu-engine/delete/{id}', 'Engine\EngineController@destroy')->name('menu-engine.delete');
+
+// CRUD MENU Engine
+Route::get('/menu-engine', 'Engine\EngineController@index')->name('menu-engine')->middleware('ceklogin');
+Route::get('/menu-engine/add', 'Engine\EngineController@add')->name('menu-engine.add')->middleware('ceklogin');
+Route::get('/menu-engine/edit/{id}', 'Engine\EngineController@edit')->name('menu-engine.edit')->middleware('ceklogin');
+Route::post('/menu-engine/save', 'Engine\EngineController@save')->name('menu-engine.save')->middleware('ceklogin');
+Route::delete('/menu-engine/delete/{id}', 'Engine\EngineController@destroy')->name('menu-engine.delete');
 
 // PENGATURAN HAK AKSES
 Route::post('/akses/insert', 'Engine\AksesController@saveInsert')->name('akses.insert')->middleware('ceklogin');
 Route::post('/akses/delete', 'Engine\AksesController@saveDelete')->name('akses.delete')->middleware('ceklogin');
 
 // CRUD ROUTER
-// Route::get('/router', 'Engine\RouterController@index')->name('router')->middleware('ceklogin');
-// Route::get('/router/add', 'Engine\RouterController@add')->name('router.add')->middleware('ceklogin');
-// Route::get('/router/edit/{id}', 'Engine\RouterController@edit')->name('router.edit')->middleware('ceklogin');
-// Route::post('/router/save', 'Engine\RouterController@save')->name('router.save')->middleware('ceklogin');
-// Route::delete('/router/delete/{id}', 'Engine\RouterController@destroy')->name('router.delete');
+Route::get('/router', 'Engine\RouterController@index')->name('router')->middleware('ceklogin');
+Route::get('/router/add', 'Engine\RouterController@add')->name('router.add')->middleware('ceklogin');
+Route::get('/router/edit/{id}', 'Engine\RouterController@edit')->name('router.edit')->middleware('ceklogin');
+Route::post('/router/save', 'Engine\RouterController@save')->name('router.save')->middleware('ceklogin');
+Route::delete('/router/delete/{id}', 'Engine\RouterController@destroy')->name('router.delete');
 
+//panggil session router
 $sessionUser = session()->get('admin_id');
 if($sessionUser != '')
 {
@@ -47,5 +54,4 @@ if($sessionUser != '')
 }else{
     
 }
-//panggil session router
 
